17839283ba90eaf2b09d8967c21d5da1c117fe23
Richell Bonfield <richell.bonfield12@gmail.com>
2020-04-13 04:01:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -22,20 +22,22 @@ function render() {
 // once the store is created, call an initial dispatch
 
 
-function candyReducer(state = [], action) {
-  switch (action.type) {
-    case 'ADD_CANDY':
-      return [...state, action.candy];
-    default:
-      return state;
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
   }
-}
-
-function render() {
-  let container = document.getElementById('container');
-  if(store.getState()) {
-    container.textContent = store.getState().join(' ')
-  } else {
-    throw new Error("the store's state has not been defined yet")
-  }
+ 
+  function getState() {
+    return state;
+  };
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+ 
 };
